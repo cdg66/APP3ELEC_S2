ce46682938dad27f0eb31a4aed3c97f5422f2c22
Felix8665 <92393635+Felix8665@users.noreply.github.com>
2022-02-08 11:55:42 -0500

    fichier.csb

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-a = np.genfromtxt('muon.txt', skip_header=2, delimiter=',', dtype=float)
+a = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Primaire.csv', delimiter=',', dtype=float)
 ax = a[:,0:2]
 plt.plot(ax)
 plt.show()
