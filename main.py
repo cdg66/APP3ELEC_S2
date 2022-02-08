06263b1891c705e2d280bbafa4253c5e9fed6325
Felix8665 <92393635+Felix8665@users.noreply.github.com>
2022-02-08 10:12:18 -0500

    Premier code qui marche

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 
 
 import numpy as np
-a = np.loadtxt('muon.txt', skiprows=1)
-s = a.strip(',')
+import matplotlib.pyplot as plt
+a = np.genfromtxt('muon.txt', skip_header=1, delimiter=',', dtype=int)
+ax = a[:,0:2]
+plt.plot(ax)
+plt.show()
+#print(ax)
+
+
+
+
+
+
+
+
+
+
+
+
+#names = (arr[0])
+#for n in range(len(names)):
+# plt.plot(arr[n], arr[n])
+#plt.legend()
+#for i in a:
+#a = [0]
+#plt.scatter(['x'], ['y'])
+#plt.plot(a)
+#plt.show()
+
+
+
+
 
 
 
