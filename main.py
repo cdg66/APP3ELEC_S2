a4400ae73e4f6e6effdab07de327690c2ae8d180
Felix8665 <92393635+Felix8665@users.noreply.github.com>
2022-02-09 15:50:35 -0500

    ajout csv + coincidence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def axis(array, colum):
     ret = np.array([])
@@ -15,23 +14,63 @@ def histogramme(x, width, plt):
     histmax = np.ceil(max(x)) + width
     bins = np.arange(histmin, histmax, width)
     plt.hist(x, bins=bins)
-#    plt.show()
+    plt.show()
 
-# reteving data
-a = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Primaire.csv', delimiter=',', dtype=float)
-# isolate axis
-index = axis(a, 0)
-temps = axis(a, 1)
-tension = axis(a, 2)
-tempsmortcumul = axis(a, 3)
-temeperatue = axis(a, 4)
 
-# data processing
+def coincidance(T1, T2, DT):
+    TR = np.zeros(len(T1))
+    dernierecoincidence = 0
+    for i in range(0, len(T1)):
 
-histogramme(tension, 100, plt)
+        for j in range(dernierecoincidence, len(T1)):
 
-#ploting
-plt.show()
+            delta = T1[i] - T2[j]
+            if delta < 0:  # depasse
+                # save 0
+                TR[i] = 0
+                break
+            if delta <= DT:  # C
+                dernierecoincidence = j
+                # save 1
+                TR[i] = 1
+                break
+            if delta >= DT:  # NC
+                pass
+
+    return TR
+
+def main():
+    # reteving datas
+    prim = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Primaire.csv', delimiter=',', dtype=float)
+    sec = np.genfromtxt('S2GE_APP3_Problematique_Detecteur_Secondaire.csv', delimiter=',', dtype=float)
+    # isolate axis
+    # index
+    index = axis(prim, 0)
+    # tension
+    prim_tension = axis(prim, 2)
+    sec_tension = axis(sec, 2)
+    # temps
+    temps_prim = axis(prim, 1)
+    temps_sec = axis(sec, 1)
+
+    # tempsmortcumul = axis(prim, 3)
+    # temeperatue = axis(prim, 4)
+
+    # data processing
+    coincidancetab = coincidance(temps_sec, temps_prim, 0.01)
+
+     histogramme(tension, 100, plt)
+
+    # ploting
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
 
 
 
